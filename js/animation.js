6fd054812c9051eb64f16eaab6cad9bcfe205ffe
cyang2 <christopheryang2019@u.northwestern.edu>
2017-10-12 13:54:09 -0500

    navbar changes color on scroll (no smooth transition effect yet)

--- js/animation.js
+++ js/animation.js
@@ -1,21 +1,18 @@
-// $(document).scroll(function() {
-//    if($(window).scrollTop() === 0) {
-//      $(".navbar").hide();
-//    } else {
-// 		 $(".navbar").show();
-// 	 }
-// });
+$(document).ready(function(){
+  var scroll_start = 0;
+  $(document).scroll(function() {
+    scroll_start = $(this).scrollTop();
+    if (scroll_start > 0) {
+      // $('#navbar').velocity({ background-color: rgba(255, 255, 255, 0.95) }, 1000);
+      // $('#navbar').velocity({ color: var(--color0) }, 1000);
 
-// $(document).ready(function() {
-//   $("html, body").animate({scrollTop: 0}, 0); //100ms for example
-// });
-
-// $(window).on('beforeunload', function() {
-//     $(window).scrollTop(0);
-// });
-
-$(document).ready(function() {
-  $("html, body").animate({ scrollTop: 0 }, 0);
-}).on('scroll', function () {
-  $('.navbar').css('opacity', ($(document).scrollTop() / 300));
+      $('#navbar').css('border-bottom', '2px solid var(--color0)');
+      $('#navbar').css('background-color', 'rgba(255, 255, 255, 0.95)');
+      $('#navbar').css('color', 'var(--color0)');
+    } else {
+      $('#navbar').css('border-bottom', '2px solid transparent');
+      $('#navbar').css('background-color', 'transparent');
+      $('#navbar').css('color', 'white');
+    }
+  });
 });
