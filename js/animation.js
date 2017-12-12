8fb577d58994db83e30c9e0741202d27d92606db
cyang2 <christopheryang2019@u.northwestern.edu>
2017-12-12 17:27:32 -0500

    comment out navbar animation for now

--- js/animation.js
+++ js/animation.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-  var url = window.location.href;
-
-  var scroll_start = 0;
-  $(document).scroll(function() {
-    scroll_start = $(this).scrollTop();
-    if (scroll_start > 100) {
-      // $('#navbar').velocity({ background-color: rgba(255, 255, 255, 0.95) }, 1000);
-      // $('#navbar').velocity({ color: var(--color0) }, 1000);
-
-      $('#navbar').css('border-bottom', '2px solid var(--color0)');
-      $('#navbar').css('background-color', 'rgba(255, 255, 255, 0.95)');
-      $('#navbar').css('color', 'var(--color0)');
-    } else {
-      $('#navbar').css('border-bottom', '2px solid transparent');
-      $('#navbar').css('background-color', 'transparent');
-
-      if (url.includes('index.html') || url.includes('team.html')) {
-        $('#navbar').css('color', 'white');
-      } else {
-        $('#navbar').css('color', 'var(--color0)');
-      }
-    }
-  });
-});
+// $(document).ready(function(){
+//   var url = window.location.href;
+//
+//   var scroll_start = 0;
+//   $(document).scroll(function() {
+//     scroll_start = $(this).scrollTop();
+//     if (scroll_start > 100) {
+//       // $('#navbar').velocity({ background-color: rgba(255, 255, 255, 0.95) }, 1000);
+//       // $('#navbar').velocity({ color: var(--color0) }, 1000);
+//
+//       $('#navbar').css('border-bottom', '2px solid var(--color0)');
+//       $('#navbar').css('background-color', 'rgba(255, 255, 255, 0.95)');
+//       $('#navbar').css('color', 'var(--color0)');
+//     } else {
+//       $('#navbar').css('border-bottom', '2px solid transparent');
+//       $('#navbar').css('background-color', 'transparent');
+//
+//       if (url.includes('index.html') || url.includes('team.html')) {
+//         $('#navbar').css('color', 'white');
+//       } else {
+//         $('#navbar').css('color', 'var(--color0)');
+//       }
+//     }
+//   });
+// });
