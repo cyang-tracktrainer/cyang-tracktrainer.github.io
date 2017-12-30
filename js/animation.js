6caaa8d44933e449a20d7239656e238a415b4088
cyang2 <christopheryang2019@u.northwestern.edu>
2017-12-30 17:40:13 -0500

    completed commit (includes renaming of classes, commenting out of certain blocks / creation of new blocks based on mockup v2)

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var url = window.location.href;
-  $("html,body").animate({ scrollTop: 0 });
+  // $("html,body").animate({ scrollTop: 0 });
   $(document).scroll(function() {
     if($(document).scrollTop() < 100) {
       $('#navbar').stop().fadeOut(300);
